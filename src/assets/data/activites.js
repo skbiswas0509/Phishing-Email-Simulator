8f4338adc2221ebadfe6270a23a843bcc202b720
skbiswas0509 <sukumarbiswas98@gmail.com>
2026-05-19 12:49:44 +0600

    done with initial dashboard

---
+++ src/assets/data/activites.js
@@ -0,0 +1,14 @@
+export const activites = [
+  "Sam entered credentials on fake login page",
+  "Max clicked phishing link in DocuSign email",
+  "Fay opened phishing simulation email",
+  "Sybo reported suspicious email to security team",
+  "Mel downloaded and opened malicious attachment",
+  "Den entered MFA code on fake portal",
+  "Egan hovered over malicious link",
+  "Ian replied to spoofed IT email",
+  "Oma submitted credentials to IT Support campaign",
+  "Tuy clicked malicious link and entered password",
+  "Qin opened suspicious password reset email",
+  "Kol clicked CEO wire transfer link",
+];
--- src/components/dashboard/ActivityFeed.jsx
+++ src/components/dashboard/ActivityFeed.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import { activites } from '../../assets/data/activites'
 
 export default function ActivityFeed() {
   return (
-    <div>ActivityFeed</div>
+    <div className='bg-zinc-900 border border-zinc-800 rounded-2xl p-5'>
+      <h3 className='font-semibold mb-5'>
+        Live Activity
+      </h3>
+
+      <div className='space-y-4'>
+        {activites.map((activity, index) => (
+          <div key={index} className='border border-zinc-800 rounded-xl px-4 py-3'>
+            {activity}
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
